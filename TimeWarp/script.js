5b6ddbe682ae0966205dcdf1f1438562cd55e916
Nathaniel Vertanus <nathanielvertanus2007@gmail.com>
2023-10-10 11:38:12 -0700

    This is the 2 week sprint commit for TimeWarp.

---
+++ TimeWarp/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the button element by its ID
+    var globeButton = document.getElementById("globeButton");
+
+    // Add a click event listener to the button
+    globeButton.addEventListener("click", function () {
+        // Redirect to the desired website
+        window.location.href = "C:\\Users\\natha\\JavaVSCode\\Projects\\TimeWarp\\TimeWarp (1).html"; // Replace with your desired URL
+    });
+});
